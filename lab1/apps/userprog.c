ccdd5c57a14ca823a4973cb1d4e6773a35217491
astpierre <astpier@purdue.edu>
2018-08-28 19:05:51 -0400

    final solution Getpid(), forking did not work

--- lab1/apps/userprog.c
+++ lab1/apps/userprog.c
@@ -2,26 +2,15 @@
 
 void main (int x)
 {
-	int fork_pid;
 	Printf("Hello World!\n");
 	Printf("LAB01: Implementing User Trap\n");
+
 	/*********************************************************
 	 * Getpid() is a system call to the OS to print the 
-	 * current process ID. I forked a new process to 
-	 * demonstrate the PID changing as the process changes.
+	 * current process ID. 
 	 ********************************************************/
 	Getpid(); /* Show current process ID */
-	fork_pid = Fork(); /* Fork another process */
-	
-	if(fork_pid < 0){/* ERROR OCCURED */ exit(-1);}
-	else if(fork_pid == 0){/* CHILD PROCESS */ Getpid();}
-	else{/* PARENT PROCESS */ 
-		Getpid();	
-		}
-	/******************************************************
-	 * Note: The parent pid should be equivalent to the 
-	 * 		 first pid called prior to forking.
-	 ******************************************************/
+
 	
 	while(1); // Use CTRL-C to exit the simulator
 }
